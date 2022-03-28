121c47bd8970b9e75e55e55e84679d15e01d9a26
Bertille Temple-Boyer-Dury <bertilletemple@yahoo.fr>
2022-03-28 12:31:03 +0200

    update data/FR to get the data file format we want

--- src/scripts/parser.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import requests
-
-src=requests.get('https://geoservices.wallonie.be/arcgis/rest/services/AGRICULTURE/SIGEC_PARC_AGRI_ANON__2020/MapServer/legend').content
-df_list = pd.read_html(src)
-predata = df_list[6].drop(columns=df_list[6].columns[0])
-data = predata.drop(labels=1, axis=0)
-data.to_csv('grpCulture_2020_Wallonie.csv')
