348d7c9764136fb68c54e80a31fd126494ab1605
Bertille Temple-Boyer-Dury <bertilletemple@yahoo.fr>
2022-03-28 10:57:47 +0200

    configurate environment variable and update tranlator script to print hello world

--- src/scripts/translator.py
+++ src/scripts/translator.py
@@ -1,5 +1,11 @@
-#translation
 import deepl
+import os
 
+# Create a Translator object providing your DeepL API authentication key.
+# To avoid writing your key in source code, you can set it in an environment
+# variable DEEPL_AUTH_KEY, then read the variable in your Python code:
+translator = deepl.Translator(os.getenv("DEEPL_AUTH_KEY"))
+
+# Translate text into a target language, in this case, French
 result = translator.translate_text("Hello, world!", target_lang="FR")
-print(result)
+print(result)  # "Bonjour, le monde !"
