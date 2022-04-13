34ca1cb3030667fc2228aef6eba6e42b7a2f40ce
Bertille Temple-Boyer-Dury <bertilletemple@yahoo.fr>
2022-04-13 17:22:42 +0200

    conversion script working with JECAM

--- src/scripts/converter_pd_cp.py
+++ src/scripts/converter_pd_cp.py
@@ -4,49 +4,39 @@ import numpy as np
 import itertools
 from fuzzywuzzy import fuzz, process
 
+#list of class names of the target data
+jcmAttributes=['GROUP_fr', 'CROPS_fr', 'SUB-CROPS_fr', 'SUB-SUB-CROPS_fr']
+
 resultList = [] # example : [['BRO',3,0.8]]
 
 srcDf = pd.read_csv('../../data/FR/FR_2020.csv')
-srcDf2 = srcDf.drop_duplicates(subset = ["GROUP_FR"])
-
 jcmDf = pd.read_csv('../../data/JECAM/JECAM_fr.csv')
 columns = list(jcmDf) 
-levelsT = []
-IDsT = []
-for c in columns: 
-    if '_fr' in c:
-        levelsT.append(c)
-    if 'ID_' in c:
-        IDsT.append(c)
-jcmLevelsDf = jcmDf[levelsT]
-jcmIDsDf = jcmDf[IDsT]
-
-print (jcmLevelsDf)
-print (jcmIDsDf)
+#print(columns)
+srcDf2 = srcDf.drop_duplicates(subset = ["GROUP_FR"])
+print(srcDf2)
 
 def scanTarget(g, idS):
-    for jcmClass in levelsT: 
+    for jcmClass in columns: 
         jcmDf2=jcmDf.drop_duplicates(subset = jcmClass)
-        idT = 'ID_' + jcmClass[:-3]
-        print(idT)
+        #print(g)
+        #print(jcmDf2[jcmClass])
         #print('--------------------------------------------scanTarget is launched')
-        print(jcmDf2.dtypes)
-        jcmDf2.apply(lambda x: matching(idS,g,x[jcmClass],jcmClass,x[idT]), axis=1)
+        jcmDf2.apply(lambda x: matching(idS,g,x[jcmClass],jcmClass), axis=1)
 
-def matching(idS,g,c,l,idT): 
+def matching(idS,g,c,l): 
     '''print('source : ')
     print(g)
     print('target : ')
     print(c)'''
     nb = 0
     #print(c)
-    if c != 'NaN' and c != 'NaN':
-        print(type(c))
+    if c != 'NaN' and type(c) == str:
         #print('--------------------------------------------fuzz_ratio is called')
         nb = fuzz.ratio(g,c)
         #rint(nb)
         if nb > 75: 
-            resultList.append([idS, g, c, l, nb, idT])
+            resultList.append([idS, g, c, l, nb])
 
 srcDf2.apply(lambda x:scanTarget(x.GROUP_FR, x.ID_GROUP_FR), axis=1)
 print(resultList)
