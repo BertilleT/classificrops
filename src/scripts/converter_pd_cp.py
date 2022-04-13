da9d376265d6ca35ba352acf171f9b37921a930e
Bertille Temple-Boyer-Dury <bertilletemple@yahoo.fr>
2022-04-13 18:43:05 +0200

    add a working converter script to icc

--- src/scripts/converter_pd_cp.py
+++ src/scripts/converter_pd_cp.py
@@ -4,39 +4,34 @@ import numpy as np
 import itertools
 from fuzzywuzzy import fuzz, process
 
-#list of class names of the target data
-jcmAttributes=['GROUP_fr', 'CROPS_fr', 'SUB-CROPS_fr', 'SUB-SUB-CROPS_fr']
-
-resultList = [] # example : [['BRO',3,0.8]]
+resultList = [] 
 
 srcDf = pd.read_csv('../../data/FR/FR_2020.csv')
-jcmDf = pd.read_csv('../../data/JECAM/JECAM_fr.csv')
-columns = list(jcmDf) 
-#print(columns)
 srcDf2 = srcDf.drop_duplicates(subset = ["GROUP_FR"])
-print(srcDf2)
+iccDf = pd.read_csv('../../data/ICC/ICC_fr.csv')
 
-def scanTarget(g, idS):
-    for jcmClass in columns: 
-        jcmDf2=jcmDf.drop_duplicates(subset = jcmClass)
-        #print(g)
-        #print(jcmDf2[jcmClass])
-        #print('--------------------------------------------scanTarget is launched')
-        jcmDf2.apply(lambda x: matching(idS,g,x[jcmClass],jcmClass), axis=1)
+#transform the code "1.34.28" into "13428" to work with integers. 
+def parserICCCode(x):
+    return "".join(x.split('.'))
 
-def matching(idS,g,c,l): 
-    '''print('source : ')
-    print(g)
-    print('target : ')
-    print(c)'''
+iccDf['code'] = iccDf['code'].apply(lambda code:parserICCCode(code))
+iccDf['code'] = iccDf['code'].astype('int')
+
+#select the first digit of the code, which corresponds to the group crop code
+iccDf['group_code'] = iccDf['code'].astype('str').str[:1].astype('int')
+
+def scanTarget(idS,g):
+    iccDf.apply(lambda x: matching(idS,g,x.label_fr,x.group_code), axis=1)
+
+def matching(idS,src,trg,idT): 
     nb = 0
-    #print(c)
-    if c != 'NaN' and type(c) == str:
-        #print('--------------------------------------------fuzz_ratio is called')
-        nb = fuzz.ratio(g,c)
-        #rint(nb)
-        if nb > 75: 
-            resultList.append([idS, g, c, l, nb])
+    nb = fuzz.ratio(src,trg)
+    if nb > 75: 
+        #resultList.append([idS, src, trg, idT, nb])
+        resultList.append([idS, idT, nb])
 
-srcDf2.apply(lambda x:scanTarget(x.GROUP_FR, x.ID_GROUP_FR), axis=1)
+#def scanSource():
+
+
+srcDf2.apply(lambda x:scanTarget(x.ID_GROUP_FR,x.GROUP_FR), axis=1)
 print(resultList)
