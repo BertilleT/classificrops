2d29f96288f021e24caa502c4e78da1480da3ea4
Bertille Temple-Boyer-Dury <bertilletemple@yahoo.fr>
2022-05-30 10:18:33 +0200

    minor changes

--- src/scripts/parser.py
+++ src/scripts/parser.py
@@ -2,37 +2,78 @@
 import argparse
 from converter import *
 from optimal_threshold import *
+import json
 
-#my_parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
-my_parser = argparse.ArgumentParser(description = 'arguments for converter tool')
+'''d = {
+    "f": "converter", 
+    "pa": "../../data/FR/FR_2020.csv", 
+    "pl": "FR", 
+    "l": "fr", 
+    "s": "token_set_ratio",
+    "t": 80
+}'''
 
-my_parser.add_argument('-f',
+class keyvalue(argparse.Action):
+    # Constructor calling
+    def __call__( self , parser, namespace,
+                 values, option_string = None):
+        setattr(namespace, self.dest, dict())
+          
+        for value in values:
+            # split it into key and value
+            key, value = value.split('=')
+            # assign into dictionary
+            getattr(namespace, self.dest)[key] = value
+
+
+#parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser = argparse.ArgumentParser(description = 'arguments for converter tool',fromfile_prefix_chars='@')
+
+parser.add_argument('-f',
                     help='the function', 
                     required = True)
 
-my_parser.add_argument('-pa',
-                    help='the path to the source classification', 
-                    required = True)
+parser.add_argument('-pa',
+                    help='the path to the source classification')
 
-my_parser.add_argument('-pl',
-                    help='the place under study, it can be a country, region, lander', 
-                    required = True)
+parser.add_argument('-pl',
+                    help='the place under study, it can be a country, region, lander')
 
-my_parser.add_argument('-l',
-                    help='the language in which the classification is written',
-                    required = True)
+parser.add_argument('-l',
+                    help='the language in which the classification is written')
 
-my_parser.add_argument('-s',
-                    help='the similarity method chosen', 
-                    required=True)
+parser.add_argument('-s',
+                    help='the similarity method chosen')
 
-my_parser.add_argument('-t', 
+parser.add_argument('-t', 
                     type = int, 
                     help='similarity threshold')
 
-# Execute parse_args()
-args = my_parser.parse_args()
+#parser.add_argument('-m', 
+#                    type=str, 
+#                    help='dictionary, instead of listing every arguments one by one')
 
+parser.add_argument('-d', type=json.loads)
+
+# adding an arguments 
+#parser.add_argument('--kwargs', 
+#                    nargs='*', 
+#                    action = keyvalue)
+#mydict = args.d
+#print(mydict)
+'''args = parser.parse_args()
+args.__dict__.update(d)'''
+
+args = parser.parse_args()
+with open('dict.json') as json_file:
+    #json to dict
+    args.__dict__.update(json.load(json_file))
+
+# Execute parse_args()
+#argparse_dict = vars(args)
+#argparse_dict.update(json_dict)
+
+print(args.s)
 print('If you read this line it means that you have provided '
     'all the parameters')
 
@@ -48,4 +89,4 @@ elif args.f == 'optimal_threshold' :
 
 #def test(**kargs):
 #    if kargs['fct'] == 'converter':
-#        converter()
+#        converter()'''
